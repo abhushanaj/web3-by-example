2c0c046c08fc919ee9905e02ff8fae7fa029bcda
Abhushan A. Joshi <49617450+abhu-A-J@users.noreply.github.com>
2024-01-05 12:17:02 +0530

    feat: update starter to have rq,viem as default (#5)

--- react-starter-template/src/main.tsx
+++ react-starter-template/src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const qc = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={qc}>
+			<ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" client={qc} />
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
